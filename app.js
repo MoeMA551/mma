8dea2ed4328859476b46fb9f15d65ae97c069428
MoeMA551 <92270796+MoeMA551@users.noreply.github.com>
2023-07-22 12:59:35 +0630

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let saying=document.querySelector(".greeting");
 let timeNow=new Date().getHours();
 console.log(timeNow);
 let greeting = timeNow >=5 && timeNow < 12 ? "Hi, Good Morning!":
-timeNow > 12 && timeNow <= 18 ? "Hi, Good Afternoon!": "Hi, Good Evening!";
+timeNow >= 12 && timeNow < 18 ? "Hi, Good Afternoon!": "Hi, Good Evening!";
 console.log(greeting);
 saying.innerHTML= `<h2>${greeting}</h2>`;
